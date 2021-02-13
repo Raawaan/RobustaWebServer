2fb5ebac5ad3dad8ea1947bfe0c3bc571e3e2610
Rawan <rawan.abdellatif.ext@orange.com>
2021-02-13 16:28:01 +0200

    -add new test, start 3 thread send different requests then assert

--- src/test/java/RobustaAppTest.java
+++ src/test/java/RobustaAppTest.java
@@ -1,14 +1,17 @@
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import util.RequestTestUtils;
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.assertTrue;
 
 public class RobustaAppTest {
 
     private RequestTestUtils requestTestUtils = new RequestTestUtils();
 
-    @Before
-    public void setup() {
+    @BeforeClass
+    public static void setup() {
         new Thread(() -> RobustaApp.main(new String[]{})).start();
     }
 
@@ -17,7 +20,6 @@ public class RobustaAppTest {
         String response = requestTestUtils.sendRequest("GET /");
         assertTrue(response.contains("200 OK"));
         assertTrue(response.contains("Boom!"));
-
     }
 
     @Test
@@ -25,7 +27,6 @@ public class RobustaAppTest {
         String response = requestTestUtils.sendRequest("GET /?custom-message=helloo");
         assertTrue(response.contains("200 OK"));
         assertTrue(response.contains("helloo"));
-
     }
 
     @Test
@@ -33,5 +34,45 @@ public class RobustaAppTest {
         String response = requestTestUtils.sendRequest("POST /?custom-message=helloo");
         assertTrue(response.contains("400 Bad request"));
         assertTrue(response.contains("Sorry we serve 'GET' requests only!"));
+    }
+
+    @Test
+    public void postGetRequest() throws Exception {
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+
+        startThread("GET /?custom-message=helloo",
+                "200 OK",
+                "helloo",
+                countDownLatch);
+
+        startThread("GET /",
+                "200 OK",
+                "Boom!",
+                countDownLatch);
+
+        startThread("POST /?custom-message=helloo",
+                "400 Bad request",
+                "Sorry we serve 'GET' requests only!",
+                countDownLatch);
+
+        boolean isCompleted = countDownLatch.await(100, TimeUnit.MILLISECONDS);
+        assertTrue(isCompleted);
+
+    }
+
+    private void startThread(String requestUri, String responseCode, String responseMsg, CountDownLatch countDownLatch) {
+        new Thread(() -> {
+            System.out.println(String.format("Thread %s started", Thread.currentThread().getName()));
+            try {
+                String response = requestTestUtils.sendRequest(requestUri);
+                assertTrue(response.contains(responseCode));
+                assertTrue(response.contains(responseMsg));
+            } catch (IOException e) {
+                System.out.println(e.getLocalizedMessage());
+            }
+            System.out.println(String.format("Thread %s will terminate", Thread.currentThread().getName()));
+            countDownLatch.countDown();
+
+        }).start();
     }
 }
